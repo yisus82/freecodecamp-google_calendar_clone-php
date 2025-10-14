4e7029af8484a05a9d5c4d1cceb349a82b9db350
yisus82 <djalma8@gmail.com>
2025-10-14 12:13:57 +0200

    Add favicon and fix title

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A courses calendar application">
-    <title>Calendar</title>
+    <title>📅 Courses Calendar 📅</title>
+    <link rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📅</text></svg>">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
   </head>
