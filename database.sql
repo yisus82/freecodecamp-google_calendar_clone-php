7fdd2f79b7f66e11a4ce89b386d77f241672f5b4
yisus82 <djalma8@gmail.com>
2025-10-15 09:39:47 +0200

    Change table name

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ CREATE DATABASE courses_calendar;
 
 USE courses_calendar;
 
-CREATE TABLE appointments (
+CREATE TABLE courses (
   id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   course_name varchar(255) NOT NULL,
   instructor_name varchar(255) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE appointments (
   created_at timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO appointments (course_name, instructor_name, start_date, end_date, start_time, end_time) VALUES
+INSERT INTO courses (course_name, instructor_name, start_date, end_date, start_time, end_time) VALUES
   ('Artificial Intelligence Bootcamp', 'Alex', '2025-10-19', '2025-10-28', '07:00:00', '10:00:00'),
   ('Data Science Workshop', 'Maria', '2025-11-01', '2025-11-10', '09:00:00', '12:00:00'),
   ('Web Development Course', 'John', '2025-11-15', '2025-11-25', '13:00:00', '16:00:00'),
